e18f120af28c430b880a5d4d9df9b23aff8dc2f4
Michael Gutteridge <michael.gutteridge@gmail.com>
2014-12-29 13:25:39 -0800

    Added documentation for config file

--- sw2srv/config.py
+++ sw2srv/config.py
@@ -1,8 +1,27 @@
+#
+# swift-switch-server configuration
+#
+# A basic configuration file.  Write as python.  Changing values here
+# requires restarting the flask server when its not run in debug mode.
+
+# LDAP CONFIGS
+
+# LDAP search base
 base = "dc=fhcrc,dc=org"
+
+# URI for LDAP server
 ldap_url = "ldaps://dc.fhcrc.org"
 
+# Run flask in debug mode
 debug = True
+
+# SSL CONFIGURATION
+
+# Use flask SSL. Not needed if this is run using Apache or other
+# web server with SSL
 use_ssl = True
+
+# Certificate and key file for Flask SSL
 cert = '/home/mrg/Work/swift-switch-account/certs/test.crt'
 key = '/home/mrg/Work/swift-switch-account/certs/test.key'
 
