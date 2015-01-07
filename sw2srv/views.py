3b1868ecd98973fdf206fd75e7ba838333b3628c
Michael Gutteridge <michael.gutteridge@gmail.com>
2015-01-07 12:46:04 -0800

    Updated Authorization Algorithm
    No longer uses "memberOf" for determining if a user is a member of the
    group or group manager- instead uses AD-specific search

--- sw2srv/views.py
+++ sw2srv/views.py
@@ -51,6 +51,17 @@ def requires_auth(f):
         return f(*args, **kwargs)
     return decorated
 
+def principal_in_group( conn, princ, group ):
+    # Is principal (DN) in group?
+    base = princ
+    filter = "(memberOf:1.2.840.113556.1.4.1941:={})".format( group )
+    scope = ldap.SCOPE_BASE
+    results = conn.search_s( base, scope, filter )
+    if len(results) == 1:
+        return True
+    else:
+        return False
+
 @server.route("/")
 def ping():
     return 'alive'
@@ -94,6 +105,16 @@ def auth(acct_name):
         r.status_code = 500
         return r
 
+    # get principal's DN
+    filter = "(sAMAccountName={})".format( username )
+    results = conn.search_s( base, scope, filter )
+    if results[0][0] is not None:
+        princ_dn = results[0][1]['distinguishedName'][0]
+    else:
+        r = jsonify( message = "Could not locate DN for user" )
+        r.status_code = 500
+        return r
+    server.logger.debug( "located DN for principal" )
 
     # get DN for requested group
     group_name = "%s_grp" % acct_name
@@ -109,23 +130,18 @@ def auth(acct_name):
 
     # check if principal has memberOf for the group
     server.logger.debug("checking %s vs %s", username, group_dn )
-    filter = "(&(userPrincipalName={}@fhcrc.org)(memberOf={}))".format(
-        username, group_dn ) 
-    server.logger.debug("%s", filter )
-    results = conn.search_s( base, scope, filter )
-    if len(results) == 2:
+    if principal_in_group( conn, princ_dn, group_dn ):
         is_ok = True
         server.logger.debug("Allowing access based on membership in group")
+    else:
+        server.logger.debug( "Principal not in group via chain" )
 
-    # get group managedBy entity
+    # check if principal is in managedBy for group
     server.logger.debug("checking %s vs %s", username, group_managed_by )
-    filter = "(&(userPrincipalName={}@fhcrc.org)(memberOf={}))".format(
-        username, group_managed_by )
-    server.logger.debug("%s", filter )
-    results = conn.search_s( base, scope, filter )
-    if len(results) == 2:
+    if principal_in_group( conn, princ_dn, group_managed_by ):
         is_ok = True
         server.logger.debug("Allowing access based on membership in managing group")
+
 
     if is_ok:
         server.logger.debug("returning credential")
@@ -144,4 +160,96 @@ def auth(acct_name):
 
     return Response( '', 403, '' )
 
+@server.route("/swift/account/access/<acct_name>/<username>")
+@requires_auth
+def verify(acct_name, username):
+    server.logger.setLevel(logging.DEBUG)
+    binddn = request.authorization.username
+    bindpw = request.authorization.password
+    is_ok = False
+
+    logging.debug( 'verifying user %s for account %s', username, acct_name )
+    # Authentication assumed good at this point... now lookup 
+    # account name/group combination in ad and check membership:
+    # format: user_f_grp
+    base = config.base
+    scope = ldap.SCOPE_SUBTREE
+    connect_as = "%s@fhcrc.org" % binddn
+    conn = ldap.initialize( config.ldap_url )
+    conn.set_option( ldap.OPT_REFERRALS, 0 )
+    try:
+        conn.simple_bind_s( connect_as, bindpw )
+    except:
+        server.logger.error(
+            'error connecting to ldap server {}'.format(config.ldap_url)
+        )
+        r = jsonify( message = 'Server Error: unable to contact ldap' )
+        r.status_code = 500
+        return r
+
+    # get principal's DN
+    filter = "(sAMAccountName={})".format( username )
+    results = conn.search_s( base, scope, filter )
+    if results[0][0] is not None:
+        princ_dn = results[0][1]['distinguishedName'][0]
+    else:
+        r = jsonify( message = "Could not locate DN for user" )
+        r.status_code = 500
+        return r
+    server.logger.debug( "located DN for principal" )
+
+    # get DN for requested group
+    group_name = "%s_grp" % acct_name
+    filter = "(&(sAMAccountName=%s)(objectCategory=group))" % group_name
+    results = conn.search_s( base, scope, filter )
+    if len(results) == 2:
+        group_dn = results[0][1]['distinguishedName'][0]
+        group_managed_by = results[0][1]['managedBy'][0]
+    else:
+        r = jsonify( message = 'No group for account' )
+        r.status_code = 404
+        return r
+
+    # check if principal has memberOf for the group
+    server.logger.debug("checking %s vs %s", username, group_dn )
+    if principal_in_group( conn, princ_dn, group_dn ):
+        is_ok = "member"
+        server.logger.debug("Allowing access based on membership in group")
+    else:
+        server.logger.debug( "Principal not in group via chain" )
+
+    # check if principal is in managedBy for group
+    server.logger.debug("checking %s vs %s", princ_dn, group_managed_by )
+    if principal_in_group( conn, princ_dn, group_managed_by ):
+        is_ok = "manager"
+        server.logger.debug(
+            "Allowing access based on membership in managing group"
+        )
+    else:
+        server.logger.debug( "Principal not in manager group" )
+
+
+    if is_ok:
+        server.logger.debug("returning credential")
+        r = ""
+        with open( config.keyfile, 'rb' ) as keyfile:
+            creds = csv.DictReader(keyfile, fieldnames = config.key_fields )
+            for cred in creds:
+                if cred['account'] == 'Swift_{}'.format(acct_name):
+                    r = jsonify(
+                        message = "User {} has access to {} as {}".format(
+                        username,acct_name, is_ok )
+                    )
+                    r.status_code = 200
+                    return r
+            if r == "":
+                r = jsonify( message='Credentials for account not found' )
+                r.status_code = 404
+                return r
+
+    r = jsonify( message = "User {} does not have access to {}".format(
+            username,acct_name)
+        )
+    r.status_code = 403
+    return r
 
