d8ac58d96dcde199c34f00ed90a9215a839871a5
Michael Gutteridge <michael.gutteridge@gmail.com>
2015-01-29 15:54:53 -0800

    removing junk

--- sw2srv/forms.py
+++ sw2srv/forms.py
@@ -1,11 +1,6 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, BooleanField
 
-class Foo(Form):
-    bar = StringField('bar')
-    baz = BooleanField('baz')
-
 class AuthHelper(Form):
-    #username = StringField('username')
     acct_name = StringField('acct_name')
 
